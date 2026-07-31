19c5040437da2a162ad1b6c7b0de7a78520da0c7
Viacheslav Miroshko <873220+cTux@users.noreply.github.com>
2026-07-31 15:29:35 +0300

    refactor(ui): normalize AppIcon props ownership (#159)

--- packages/ui/src/atoms/AppIcon/AppIcon.tsx
+++ packages/ui/src/atoms/AppIcon/AppIcon.tsx
@@ -1,8 +1,8 @@
 import './styles.scss';
-import { type AppIconProps } from './app-icon-props';
+import { type AppIconProps } from './types';
 
 function AppIcon({ className = 'app-icon' }: AppIconProps) {
   return <img className={className} src="./owl.svg" alt="" />;
 }
 
-export { type AppIconProps, AppIcon };
+export { AppIcon };
